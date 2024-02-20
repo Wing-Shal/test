2e6507025d3dc7bedfdf14d3b677e4294bcd7af1
simcat13 <rycbar0@gmail.com>
2024-02-20 17:11:16 +0900

    테스트

--- test/src/main/java/com/kh/test/controller/TestController.java
+++ test/src/main/java/com/kh/test/controller/TestController.java
@@ -3,5 +3,7 @@ package com.kh.test.controller;
 public class TestController {
 	public static void main(String[] args) {
 		System.out.println("테스트 - 예림");
+		System.out.println("테스트 _ 현정");
 	}
+	
 }
