393c727fc1e32f97c23189845981128c64b4d7cc
ggingggang05 <ggingggang05@gmail.com>
2024-02-20 17:16:08 +0900

    수정- 예림

--- test/src/main/java/com/kh/test/dto/TestDto.java
+++ test/src/main/java/com/kh/test/dto/TestDto.java
@@ -7,4 +7,25 @@ public class TestDto
 	private int testNo;
 	private String testName;
 	private Date upDate;
+	public int getTestNo() {
+		return testNo;
+	}
+	public void setTestNo(int testNo) {
+		this.testNo = testNo;
+	}
+	public String getTestName() {
+		return testName;
+	}
+	public void setTestName(String testName) {
+		this.testName = testName;
+	}
+	public Date getUpDate() {
+		return upDate;
+	}
+	public void setUpDate(Date upDate) {
+		this.upDate = upDate;
+	}
+	
+	
+	
 }
